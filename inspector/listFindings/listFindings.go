ff2e148710e73db37e712b1e6d4fa67ba6b70c18
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2020-07-13 14:22:45 +0900

    output findings

--- inspector/listFindings/listFindings.go
+++ inspector/listFindings/listFindings.go
@@ -10,27 +10,15 @@ import (
 	"github.com/aws/aws-sdk-go/service/inspector"
 )
 
-type arns struct {
-	arn string
-}
-
-func (a arn) String() string {
-	return fmt.Sprintf("%v", a.arn)
-}
-
-func main() {
-	svc := inspector.New(session.New(&aws.Config{
-		Region:      aws.String("ap-northeast-1"),
-		Credentials: credentials.NewSharedCredentials("", "readyfor"),
-	}))
+func listFindings(svc *inspector.Inspector, arn string) (list *inspector.ListFindingsOutput) {
 	input := &inspector.ListFindingsInput{
 		AssessmentRunArns: []*string{
-			aws.String("arn:aws:inspector:ap-northeast-1:513123329229:target/0-8wfvNcpK/template/0-R2L5fYYr/run/0-4zTKz1hJ"),
+			aws.String(arn),
 		},
 		MaxResults: aws.Int64(123),
 	}
 
-	result, err := svc.ListFindings(input)
+	list, err := svc.ListFindings(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
@@ -46,20 +34,47 @@ func main() {
 				fmt.Println(aerr.Error())
 			}
 		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
 			fmt.Println(err.Error())
 		}
 		return
 	}
+	return list
+}
 
-	var arns []*arn
-	fmt.Printf("%v", arns)
+func describeFindings(svc *inspector.Inspector, list *inspector.ListFindingsOutput) {
+	for _, i := range list.FindingArns {
+		input := &inspector.DescribeFindingsInput{
+			FindingArns: []*string{
+				aws.String(*i),
+			},
+		}
 
-	// fmt.Printf("%v \n", result.FindingArns)
-	// jsonBytes := []*string(result.FindingArns)
-	// fmt.Printf("%+v\n", &result.FindingArns)
-	// var output Output
-	// json.Unmarshal(jsonBytes, &output)
-	// fmt.Println(output.arn)
+		result, err := svc.DescribeFindings(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case inspector.ErrCodeInternalException:
+					fmt.Println(inspector.ErrCodeInternalException, aerr.Error())
+				case inspector.ErrCodeInvalidInputException:
+					fmt.Println(inspector.ErrCodeInvalidInputException, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+				fmt.Println(err.Error())
+			}
+		}
+		fmt.Println(result.Findings)
+	}
+}
+
+func main() {
+	svc := inspector.New(session.New(&aws.Config{
+		Region:      aws.String("ap-northeast-1"),
+		Credentials: credentials.NewSharedCredentials("", "readyfor"),
+	}))
+
+	list := listFindings(svc, "arn:aws:inspector:ap-northeast-1:513123329229:target/0-8wfvNcpK/template/0-R2L5fYYr/run/0-4zTKz1hJ")
+
+	describeFindings(svc, list)
 }
